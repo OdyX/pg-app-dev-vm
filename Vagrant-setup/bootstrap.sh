c0550af5659a486f6083bb5446b00ef65226fe5a
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2014-06-19 11:25:11 +0200

    Let this user create databases for test purposes

--- Vagrant-setup/bootstrap.sh
+++ Vagrant-setup/bootstrap.sh
@@ -72,6 +72,9 @@ cat << EOF | su - postgres -c psql
 -- Create the database user:
 CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
 
+-- Grant it the rights to create new databases
+ALTER USER $APP_DB_USER CREATEDB;
+
 -- Create the database:
 CREATE DATABASE $APP_DB_NAME WITH OWNER $APP_DB_USER;
 EOF
